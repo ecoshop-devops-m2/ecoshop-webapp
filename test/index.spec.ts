da784a8620800489fd06f7b2384ed1028f379f78
Vincent Petit <vincent.petit@efrei.net>
2023-02-15 19:53:50 +0100

    test of webhook

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,8 @@
-import {describe, expect, test} from "@jest/globals";
-import {run} from "../src";
+import { describe, expect, test } from "@jest/globals";
+import { run } from "../src";
 
-describe('helloTest', () => {
-    test('expect run to return Hello World msg', () => {
-        expect(run()).toMatch('Hello World !');
-    });
+describe("helloTest", () => {
+	test("expect run to return Hello World msg", () => {
+		expect(run()).toMatch("Hello World!");
+	});
 });
